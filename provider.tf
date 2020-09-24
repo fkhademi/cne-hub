ebc7edc27a6aca272c4c28878fe32828c2aea273
Frey Khademi <frey.khademi@gmail.com>
2020-09-24 16:34:11 +0200

    update

--- provider.tf
+++ provider.tf
@@ -1,16 +1,20 @@
-/* provider "aviatrix" {
+provider "aviatrix" {
   controller_ip           = var.aviatrix_controller_ip
   username                = var.aviatrix_admin_account
   password                = var.aviatrix_admin_password
   skip_version_validation = false
   version                 = ">=2.16.0"
 }
-
+provider "aws" {
+  region     = var.hub_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
 provider "dns" {
     update {
       server        = "8.8.8.8"
     }
-} */
+}
 provider "aws" {
   alias      = "dns"
   region     = var.hub_region
--- var.tf
+++ var.tf
@@ -1,4 +1,4 @@
-#Aviatrix controller vars
+/*#Aviatrix controller vars
 variable "aviatrix_admin_account" { default = "admin" }
 variable "aviatrix_admin_password" { default = "Avi12345-" }
 variable "aviatrix_controller_ip" { default = "controller.avxlab.de" }
@@ -16,3 +16,56 @@ variable "hub_region" { default = "eu-central-1" }
 variable "ssh_key" { }
 variable "aws_access_key_dns" { }
 variable "aws_secret_key_dns" { }
+*/
+#Aviatrix controller vars
+variable "aviatrix_admin_account" { 
+    description = "Aviatrix admin user"
+    default     = "admin" 
+}
+variable "aviatrix_admin_password" { 
+    description = "Aviatrix admin password"
+}
+variable "aviatrix_controller_ip" { 
+    description = "Aviatrix Controller IP/Hostname"
+}
+variable "aws_account_name" { 
+    description = "AWS Account name defined in AVX Controller"
+    default = "aws-fk"
+}
+
+# AWS account for deploying EC2 Instance
+variable "aws_access_key" { 
+    description = "AWS Access Key"
+}
+variable "aws_secret_key" { 
+    description = "AWS Secret Key"
+}
+
+# AWS account for adding Route53 hostnames
+variable "aws_access_key_dns" { 
+    description = "AWS Access Key to update Route53"
+}
+variable "aws_secret_key_dns" { 
+    description = "AWS Secret Key to update Route53"
+}
+
+# CNE details
+variable "num_pods" { 
+    description = "Number of Pods deployed"
+    default = 1 
+}
+variable "domain_name" { 
+    description = "Public Route53 Domain to update"
+    default = "pub.avxlab.nl" 
+}
+variable "priv_domain_name" { 
+    description = "Private Route53 Domain to update"
+    default = "priv.avxlab.nl" 
+}
+variable "hub_region" { 
+    description = "AWS region to deploy resources"
+    default = "eu-central-1" 
+}
+variable "ssh_key" { 
+    description = "SSH public key for test Ubuntu VM"
+}
